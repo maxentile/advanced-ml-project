95a29709f2dbec0d0cd86e90bcc1f8ed9471be8f
Josh Fass <jf694@cornell.edu>
2015-05-09 12:02:05 -0400

    starting spade inconsistency tests

---
+++ scripts/spade_inconistency.py
@@ -0,0 +1,21 @@
+from spade import SPADE
+import numpy as np
+import numpy.random as npr
+
+def main():
+    # generate synthetic datasets:
+    # (1) mixture of gaussians on a square (2D)
+    # (2) branching "X" (2D)
+    # (3) longer branching "X" (2D)
+    # (4-6) 10D variants
+    # (7-9) 50D variants
+
+
+
+    # run SPADE 9 times for each, using radial and k-nearest-based density estimators
+
+    # save results as multi-panel figures
+
+
+if __name__ == '__main__':
+    main()
--- scripts/synthetic_data.py
+++ scripts/synthetic_data.py
@@ -3,15 +3,21 @@ import numpy as np
 import numpy.random as npr
 import itertools
 
-def generate_blobs(num_samples=5000,separation=8):
-    centers = np.array([[0,0],[1,0],[0,1],[1,1]],dtype=float)
-    centers -= 0.5
-    centers = np.vstack((centers,#centers*2,centers*3,
+def hypercube(ndim=2):
+    corners = [-1,1]
+    corner_list = [corners for _ in xrange(ndim)]
+    return np.array([i for i in itertools.product(*corner_list)])
+
+def generate_blobs(num_samples=5000,separation=8,ndim=2):
+#    centers = np.array([[0,0],[1,0],[0,1],[1,1]],dtype=float)
+#    centers -= 0.5
+#    centers = np.vstack((centers,#centers*2,centers*3,
                          #centers*4,centers*5,centers*6,
                          #centers*7,centers*8,centers*9,
                          #centers*10,centers*11,centers*12,
                          #centers*13,centers*14,
-                         [0,0]))
+#                         [0,0]))
+    centers = hypercube(ndim)
     centers *= separation
     kde = KernelDensity()
     kde.fit(centers)
@@ -19,17 +25,12 @@ def generate_blobs(num_samples=5000,separation=8):
     density = np.exp(kde.score_samples(samples))
     return samples,density
 
-
 def generate_branches(num_samples=5000,
                       branch_width=0.5,
-                      branch_length=5):
+                      branch_length=5,
+                      ndim=2):
 
-    def hypercube(ndim=3):
-        corners = [-1,1]
-        corner_list = [corners for _ in xrange(ndim)]
-        return [i for i in itertools.product(*corner_list)]
-
-    h_corners = np.array(hypercube())*branch_length
+    h_corners = np.array(hypercube(ndim))*branch_length
     branches = np.array(h_corners)
     for i in range(20):
         branches = np.vstack((branches, h_corners*(i+2)))
